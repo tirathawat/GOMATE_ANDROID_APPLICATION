9b5dfa5d8edcc46f0534cd04fc9399bef76601f0
Thirathawat Chanserikorn <tirathawat@gmail.com>
2020-10-25 01:07:21 +0700

    create UI

---
+++ app/src/main/java/com/example/gomate/StartActivity.java
@@ -0,0 +1,42 @@
+package com.example.gomate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button button_login;
+    TextView text_sign_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        button_login = findViewById(R.id.button_login);
+        text_sign_up = findViewById(R.id.text_sign_up);
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+
+        text_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
