14847ec4712ba0fcb8d30ad204bf3d6a856f6caa
gimmeabeer <53146540+krastomer@users.noreply.github.com>
2020-10-30 22:30:55 +0700

    add frameLayout, Fragment home

--- app/src/main/java/com/example/gomate/MainActivity.java
+++ app/src/main/java/com/example/gomate/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gomate;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +12,40 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.gomate.fragment.home.ChatFragment;
+import com.example.gomate.fragment.home.HomeFragment;
+import com.example.gomate.fragment.home.ProfileFragment;
+import com.example.gomate.fragment.home.WalletFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView t;
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.bnv_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.bnv_wallet:
+                            selectedFragment = new WalletFragment();
+                            break;
+                        case R.id.bnv_chats:
+                            selectedFragment = new ChatFragment();
+                            break;
+                        case R.id.bnv_user:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, selectedFragment).commit();
+                    return true;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bnv_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, new HomeFragment()).commit();
     }
 }
