6f89ea3012750cfcfcfa76d3873b1b91e509127d
gan <gan131201@gmail.com>
2020-10-31 09:37:24 +0700

    add validate

---
+++ app/src/main/java/com/example/gomate/ValidateActivity.java
@@ -0,0 +1,17 @@
+package com.example.gomate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValidateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate);
+    }
+    void gan(){
+        System.out.println("GanGanGanGan");
+    }
+}
