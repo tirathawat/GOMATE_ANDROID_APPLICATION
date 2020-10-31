3bcf4e742a675e14a7c9fe61fa840a2d371c3a59
gimmeabeer <53146540+krastomer@users.noreply.github.com>
2020-10-31 15:01:11 +0700

    add API keys

--- app/src/main/java/com/example/gomate/fragment/home/HomeFragment.java
+++ app/src/main/java/com/example/gomate/fragment/home/HomeFragment.java
@@ -3,64 +3,61 @@ package com.example.gomate.fragment.home;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gomate.R;
+import com.example.gomate.fragment.home.adsbanner.BannerAdapter;
+import com.example.gomate.fragment.home.adsbanner.BannerItem;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private BannerAdapter bannerAdapter;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        setupBannerItems();
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewPager2 viewPager2 = view.findViewById(R.id.tv_banner);
+//        viewPager2.setAdapter(bannerAdapter);
+        return  view;
+
+    }
+
+    private void setupBannerItems(){
+        List<BannerItem> bannerItems = new ArrayList<>();
+
+        BannerItem itemFirst = new BannerItem();
+        itemFirst.setTextShow("item 1");
+        BannerItem itemSecond = new BannerItem();
+        itemSecond.setTextShow("item 2");
+        BannerItem itemThird = new BannerItem();
+        itemThird.setTextShow("item 3");
+
+        bannerItems.add(itemFirst);
+        bannerItems.add(itemSecond);
+        bannerItems.add(itemThird);
+
+        BannerAdapter bannerAdapter = new BannerAdapter(bannerItems);
+
     }
 }
---
+++ app/src/main/java/com/example/gomate/fragment/home/adsbanner/BannerItem.java
@@ -0,0 +1,13 @@
+package com.example.gomate.fragment.home.adsbanner;
+
+public class BannerItem {
+    private String textShow;
+
+    public String getTextShow() {
+        return textShow;
+    }
+
+    public void setTextShow(String textShow) {
+        this.textShow = textShow;
+    }
+}
